f16b1263bad792b3a26ec43595a9f26416eedeab
wenxiu.su <wenxiu.su@beibei.com>
2021-05-24 20:11:50 +0800

    11

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); // 压缩js文件
 
 const common = require('./webpack.common.js');
 
@@ -9,7 +9,7 @@ module.exports = merge(common, {
     plugins: [
         new UglifyJSPlugin({
             // sourceMap: true,
-        }),
+        }), // Uglify：丑化 
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         })
